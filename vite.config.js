1825e5fa3f43d0f21f0e85ad8d09e4895b425837
Brayden Williams <1311325+redstar504@users.noreply.github.com>
2024-01-18 17:22:55 -0800

    init commit

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import vituum from 'vituum'
+import twig from '@vituum/vite-plugin-twig'
+
+export default {
+    plugins: [
+        vituum(),
+        twig({
+            root: './src',
+        })
+    ]
+}
